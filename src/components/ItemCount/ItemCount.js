716b9b3e0b50f126fa86947f0add9fecaa09477c
christian.guzman1 <christiaan.guzman@geniussportsmedia.com>
2021-06-14 09:43:10 -0500

    set 1 like minimum parameter

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -11,7 +11,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function ItemCount() {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(1)
     const classes = useStyles();
 
  return (
