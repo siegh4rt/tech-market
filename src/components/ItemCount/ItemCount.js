42741118ac099fab1c6a756d6d0cc0750ee89fa1
christian.guzman1 <christiaan.guzman@geniussportsmedia.com>
2021-06-14 09:37:24 -0500

    add useState to ItemCount component

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      marginTop: theme.spacing(2)
+    },
+}));
+
+export default function ItemCount() {
+    const [count, setCount] = useState(0)
+    const classes = useStyles();
+
+ return (
+   <Grid container className={classes.root} spacing={2}>
+    <Button onClick={() => setCount(count + 1)}>+</Button>
+    <div>{count}</div>
+    <Button onClick={() => setCount(count - 1)}>+</Button>
+   </Grid>
+ )
+}
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import ItemCount from '../ItemCount/ItemCount';
 
 export default function ItemListContainer() {
- return <>soy listContainer</>
+    return <ItemCount />
 }
